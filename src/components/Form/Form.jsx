cc245c8ce683f39dcf7dc58a08e924c8a3cf49fb
nforn22 <fornarinicolas22@gmail.com>
2025-05-29 17:00:41 +0200

    bonus: add password visibility toggle and mismatch warning password input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
 const Form = ({ userData, onChange, onSubmit })  => {
+        const [showPassword, setShowPassword] = useState(false);
+        const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+        const isMismatch =
+        userData.password &&
+        userData.confirmPassword &&
+        userData.password !== userData.confirmPassword;
+
     return (
         <form onSubmit={onSubmit}>
             <input 
@@ -17,22 +28,42 @@ const Form = ({ userData, onChange, onSubmit })  => {
                 onChange={onChange}
                 required
             />
+            <div className="password-container">
             <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="Password"
                 name="password"
                 value={userData.password}
                 onChange={onChange}
                 required
+                className={isMismatch ? "error" : ""}
             />
+            <span
+                className="toggle-eye"
+                onClick={() => setShowPassword((prev) => !prev)}
+            >
+                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </span>
+        </div>
+
+        <div className="password-container">
             <input
-                type="password"
+                type={showConfirmPassword ? "text" : "password"}
                 name="confirmPassword"
                 placeholder="Confirm Password"
                 value={userData.confirmPassword}
                 onChange={onChange}
                 required
+                className={isMismatch ? "error" : ""}
             />
+            <span
+                className="toggle-eye"
+                onClick={() => setShowConfirmPassword((prev) => !prev)}
+            >
+                {showConfirmPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </span>
+        </div>
+
             <button type="submit">Register</button>   
         </form>
     );
